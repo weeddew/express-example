8555811401caa73a1675b95ddd53171736f1a0e1
hslee <hslee@koreaap.com>
2022-04-08 17:20:11 +0900

    test

--- express-example.js
+++ express-example.js
@@ -1,16 +1,34 @@
 
-
 const express = require('express')
 const app = express()
 const port = 3000
 
+const { createLogger, format, transports } = require('winston');
+const { combine, timestamp, printf } = format;
+
+const myFormat = printf(({ level, message, label, timestamp }) => {
+    return `${timestamp} ${level}: ${message}`;
+});
+
+const logger = createLogger({
+    format: combine(
+        timestamp(),
+        myFormat
+    ),
+    transports: [
+        new transports.File({ filename: 'server.log' }),
+    ],
+});
+
 app.get('/', (req, res) => {
     console.log(`request : ${ new Date()}`)
+    logger.info(`request : /}`);
     res.send('Hello World!')
 })
 
 app.get('/:id', (req, res) => {
     console.log(`request ${req.params.id} : ${ new Date()}`)
+    logger.info(`request /${req.params.id} `);
     res.send('Hello World!' +  req.params.id)
 })
 
