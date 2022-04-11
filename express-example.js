b9e96d8f7ffb1237c776d1e1303214f119fe3089
hslee <hslee@koreaap.com>
2022-04-11 13:19:33 +0900

    test

--- express-example.js
+++ express-example.js
@@ -1,7 +1,40 @@
+var dotenv = require('dotenv');
+dotenv.config();
 
 const express = require('express')
 const app = express()
 const port = 3000
+
+var mongoose = require('mongoose');
+
+//Set up default mongoose connection
+var mongoDB = process.env.MONGODB_URI;
+mongoose.connect(mongoDB, {
+    dbName: 'sample_analytics'
+});
+// Get Mongoose to use the global promise library
+mongoose.Promise = global.Promise;
+//Get the default connection
+var db = mongoose.connection;
+
+//Bind connection to error event (to get notification of connection errors)
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+
+
+//Define a schema
+var Schema = mongoose.Schema;
+
+var CustomersSchema = new Schema({
+    username: String,
+    birthdate: Date
+});
+
+var Customers = mongoose.model('Customers', CustomersSchema );
+
+
+// sample_analytics.customers
+
 
 const { createLogger, format, transports } = require('winston');
 const { combine, timestamp, printf } = format;
@@ -21,6 +54,15 @@ const logger = createLogger({
 });
 
 app.get('/', (req, res) => {
+    Customers.find({ 'username': { $regex: '.*' + 'er' + '.*' } }, 'username name address', function (err, customers) {
+        if (err) {
+            console.log(`not found`)
+        };
+
+        console.log(customers)
+    })
+
+
     console.log(`request : ${ new Date()}`)
     logger.info(`request : /`);
     res.send('Hello World!')
